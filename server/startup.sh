6799dbe9f8f038d008bdd4ec7744698f7c1be46a
albanD <alban@robots.ox.ac.uk>
2017-01-11 21:21:10 +0000

    make sure tensorboard refresh more often than once every minute!

--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,4 @@
 # TODO remove the stdbuf in final version if not needed
-stdbuf -i0 -o0 -e0 tensorboard --logdir /tmp/tensorboard --port 8888 2>&1 1>/tmp/tensorboard.log &
+stdbuf -i0 -o0 -e0 tensorboard --reload_interval 1 --logdir /tmp/tensorboard --port 8888 2>&1 1>/tmp/tensorboard.log &
 
 stdbuf -i0 -o0 -e0 python /server.py 8889 2>&1 1>/tmp/server.log
