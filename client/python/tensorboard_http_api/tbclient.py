e2ea91be188fe7cb4972a0648aa02ea2615215d9
Nantas Nardelli <nantas.nardelli@gmail.com>
2017-01-14 16:26:28 +0000

    Draft of implementation for interface

--- client/python/tensorboard_http_api/tbclient.py
+++ client/python/tensorboard_http_api/tbclient.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class TBClient(object):
@@ -6,25 +7,63 @@ class TBClient(object):
     def __init__(self, hostname, port):
         self.hostname = hostname
         self.port = port
-        pass
+        self.url = self.hostname + ":" + str(self.port)
+        # TODO use urlparse
+        if not (self.url.startswith("http://") or
+                self.url.startswith("https://")):
+            self.url = "http://" + self.url
+
+        # check server is up. Might be a bit costly, so we should find a better
+        # method to do it.
+        assert(requests.get(self.url).ok)
 
     def get_experiments(self, xp=None):
-        pass
+        query = "/data"
+        r = requests.get(self.url + query)
+        if not r.ok:
+            experiments = []
+        else:
+            experiments = json.loads(r.text)
+        return experiments
 
     def add_scalar(self, xp, name, data):
-        pass
+        query = "/data/scalars?xp={}&name={}".format(xp, name)
+        r = requests.post(self.url + query, json=data)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
 
     def get_scalars(self, xp, name):
-        pass
+        query = "/data/scalars?xp={}&name={}".format(xp, name)
+        r = requests.get(self.url + query)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+        return r.text
 
     def add_histogram(self, xp, name, tobuild, data):
-        pass
+        query = "/data/histograms?xp={}&name={}&tobuild={}".format(
+            xp, name, tobuild)
+        r = requests.post(self.url + query, json=data)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
 
     def get_histograms(self, xp, name):
-        pass
-
-    def get_data(self, xp):
-        pass
+        query = "/data/histograms?xp={}&name={}".format(xp, name)
+        r = requests.get(self.url + query)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+        return r.text
 
     def set_data(self, xp, force, data):
-        pass
+        query = "/data/backup?xp={}&force={}".format(
+            xp, force)
+        r = requests.post(self.url + query, json=data)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+
+    def get_data(self, xp):
+        query = "/data/backup?xp={}".format(
+            xp, force)
+        r = requests.get(self.url + query)
+        if not r.ok:
+            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+        return r.text
