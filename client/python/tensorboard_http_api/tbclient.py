2581273d96dd57d290b38125f56f9aab56a7ed17
Nantas Nardelli <nantas.nardelli@gmail.com>
2017-01-16 22:13:55 +0000

    Add histogram interface and (partial) tests for it

--- client/python/tensorboard_http_api/tbclient.py
+++ client/python/tensorboard_http_api/tbclient.py
@@ -38,40 +38,59 @@ class TBClient(object):
         query = "/data/scalars?xp={}&name={}".format(xp, name)
         r = requests.post(self.url + query, json=data)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+            raise ValueError("Something went wrong.")
 
     def get_scalars(self, xp, name):
         query = "/data/scalars?xp={}&name={}".format(xp, name)
         r = requests.get(self.url + query)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
-        return r.text
+            raise ValueError("Something went wrong.")
+        return json.loads(r.text)
 
-    def add_histogram(self, xp, name, tobuild, data):
+    def add_histogram(self, xp, name, data, tobuild=False):
+        assert(len(data) == 3)
+        if not self.check_histogram_data(data[2], tobuild):
+            raise ValueError("Data was not provided in a valid format!")
         query = "/data/histograms?xp={}&name={}&tobuild={}".format(
-            xp, name, tobuild)
+            xp, name, str(tobuild).lower())
         r = requests.post(self.url + query, json=data)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+            raise ValueError("Something went wrong.")
 
     def get_histograms(self, xp, name):
         query = "/data/histograms?xp={}&name={}".format(xp, name)
         r = requests.get(self.url + query)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
-        return r.text
+            raise ValueError("Something went wrong.")
+        return json.loads(r.text)
 
     def set_data(self, xp, force, data):
         query = "/data/backup?xp={}&force={}".format(
             xp, force)
         r = requests.post(self.url + query, json=data)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+            raise ValueError("Something went wrong.")
 
     def get_data(self, xp):
-        query = "/data/backup?xp={}".format(
-            xp, force)
+        query = "/data/backup?xp={}".format(xp)
         r = requests.get(self.url + query)
         if not r.ok:
-            raise ValueError("SOMETHING HORRIBLY WRONG HAS HAPPENED")
+            raise ValueError("Something went wrong.")
         return r.text
+
+    def check_histogram_data(self, data, tobuild):
+        if tobuild:
+            return len(data) == 7
+        # TODO should use a schema here
+        # Note: all of these are sorted already
+        expected = ["bucket", "bucket_limit", "max", "min", "num"]
+        expected2 = ["bucket", "bucket_limit", "max", "min", "num",
+                     "sum"]
+        expected3 = ["bucket", "bucket_limit", "max", "min", "num",
+                     "sum", "sum_squares"]
+        expected4 = ["bucket", "bucket_limit", "max", "min", "num",
+                     "sum_squares"]
+        ks = tuple(data.keys())
+        ks = sorted(ks)
+        return (ks == expected or ks == expected2
+                or ks == expected3 or ks == expected4)
--- client/python/test/test_tbclient.py
+++ client/python/test/test_tbclient.py
@@ -10,15 +10,24 @@ class TBClientTestSuite(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TBClientTestSuite, self).__init__(*args, **kwargs)
 
+    def setUp(self):
+        self.h = Helper()
+
+    def tearDown(self):
+        self.h.kill_remove()
+        self.h = None
+
+    # INIT
     def test_init(self):
         TBClient()
 
     def test_init_wrong_localhost(self):
-        self.failUnlessRaises(ValueError, TBClient, "not_open", 8889)
+        self.assertRaises(ValueError, TBClient, "not_open", 8889)
 
     def test_init_wrong_port(self):
-        self.failUnlessRaises(ValueError, TBClient, "localhost", 123412341234)
+        self.assertRaises(ValueError, TBClient, "localhost", 123412341234)
 
+    # scalars
     def test_add_scalar(self):
         tbc = TBClient()
         data = [time.clock(), 1, 2]
@@ -27,34 +36,198 @@ class TBClientTestSuite(unittest.TestCase):
     def test_add_scalar_less_data(self):
         tbc = TBClient()
         data = []
-        self.failUnlessRaises(AssertionError, tbc.add_scalar,
-                              "foo", "bar", data)
+        self.assertRaises(AssertionError, tbc.add_scalar,
+                          "foo", "bar", data)
 
     # TODO These should really be tested singularly...
     def test_add_scalar_wrong_data(self):
         tbc = TBClient()
         data = ["lol", "lulz", "lelz"]
-        self.failUnlessRaises(ValueError, tbc.add_scalar,
-                              "foo", "bar", data)
+        self.assertRaises(ValueError, tbc.add_scalar,
+                          "foo", "bar", data)
 
     def test_add_scalar_wrong_experiment(self):
         tbc = TBClient()
         data = [time.clock(), 1, 2]
-        self.failUnlessRaises(ValueError, tbc.add_scalar,
-                              "", "bar", data)
+        self.assertRaises(ValueError, tbc.add_scalar,
+                          "", "bar", data)
 
     def test_add_scalar_wrong_variable(self):
         tbc = TBClient()
         data = [time.clock(), 1, 2]
-        self.failUnlessRaises(ValueError, tbc.add_scalar,
-                              "foo", "", data)
+        self.assertRaises(ValueError, tbc.add_scalar,
+                          "foo", "", data)
 
-    def setUp(self):
-        self.h = Helper()
+    def test_get_scalars_no_data(self):
+        tbc = TBClient()
+        self.assertRaises(ValueError, tbc.get_scalars, "foo", "bar")
 
-    def tearDown(self):
-        self.h.kill_remove()
-        self.h = None
+    def test_get_scalars_one_datum(self):
+        tbc = TBClient()
+        tbc.add_scalar("foo", "bar", [0, 0, 0])
+        time.sleep(1)
+        self.assertEqual(tbc.get_scalars("foo", "bar"), [[0.0, 0, 0.0]])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_scalars_two_data(self):
+        tbc = TBClient()
+        tbc.add_scalar("foo", "bar", [0, 0, 0])
+        tbc.add_scalar("foo", "bar", [1, 1, 1])
+        time.sleep(1)
+        self.assertEqual(tbc.get_scalars("foo", "bar"),
+                         [[0.0, 0, 0.0], [1.0, 1, 1.0]])
+
+    def test_get_scalars_wrong_experiment(self):
+        tbc = TBClient()
+        tbc.add_scalar("foo", "bar", [0, 0, 0])
+        time.sleep(1)
+        self.assertRaises(ValueError, tbc.get_scalars, "", "bar")
+
+    def test_get_scalars_wrong_variable(self):
+        tbc = TBClient()
+        tbc.add_scalar("foo", "bar", [0, 0, 0])
+        time.sleep(1)
+        self.assertRaises(ValueError, tbc.get_scalars, "foo", "")
+
+    # Histograms
+    def test_add_histogram(self):
+        tbc = TBClient()
+        data = [time.clock(), 1, 2]
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [0, 1, data])
+
+    def test_add_histogram_with_sum(self):
+        tbc = TBClient()
+        data = [time.clock(), 1, 2]
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25],
+                "sum": 75}
+        tbc.add_histogram("foo", "bar", [0, 1, data])
+
+    def test_add_histogram_with_sumsq(self):
+        tbc = TBClient()
+        data = [time.clock(), 1, 2]
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25],
+                "sum_squares": 5625}
+        tbc.add_histogram("foo", "bar", [0, 1, data])
+
+    def test_add_histogram_with_sum_sumsq(self):
+        tbc = TBClient()
+        data = [time.clock(), 1, 2]
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25],
+                "sum": 75,
+                "sum_squares": 2675}
+        tbc.add_histogram("foo", "bar", [0, 1, data])
+
+    def test_add_histogram_less_data(self):
+        tbc = TBClient()
+        data = {"some data": 0}
+        self.assertRaises(AssertionError, tbc.add_histogram,
+                          "foo", "bar", [data])
+
+    # TODO These should really be tested singularly...
+    def test_add_histogram_wrong_data(self):
+        tbc = TBClient()
+        data = ["lolz", "lulz", "lelz"]
+        self.assertRaises(ValueError, tbc.add_histogram,
+                          "foo", "bar", [0, 1, data], True)
+
+    def test_add_histogram_wrong_experiment(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        self.assertRaises(ValueError, tbc.add_histogram,
+                          "", "bar", [0, 1, data])
+
+    def test_add_histogram_wrong_variable(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        self.assertRaises(ValueError, tbc.add_histogram,
+                          "foo", "", [0, 1, data])
+
+    def test_get_histograms_no_data(self):
+        tbc = TBClient()
+        self.assertRaises(ValueError, tbc.get_histograms, "foo", "bar")
+
+    def test_get_histograms_one_datum(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [0, 0, data])
+        time.sleep(1)
+        self.assertEqual(tbc.get_histograms("foo", "bar"),
+                         [[0.0, 0,
+                           [0.0, 100.0, 3.0, 0.0, 0.0,
+                            [10.0, 50.0, 30.0],
+                            [5.0, 45.0, 25.0]]]])
+
+    def test_get_histograms_two_data(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [0, 0, data])
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [1, 1, data])
+        time.sleep(1)
+        self.assertEqual(tbc.get_histograms("foo", "bar"),
+                         [[0.0, 0,
+                           [0.0, 100.0, 3.0, 0.0, 0.0,
+                            [10.0, 50.0, 30.0],
+                            [5.0, 45.0, 25.0]]],
+                          [1.0, 1,
+                           [0.0, 100.0, 3.0, 0.0, 0.0,
+                            [10.0, 50.0, 30.0],
+                            [5.0, 45.0, 25.0]]]])
+
+    def test_get_histograms_wrong_experiment(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [0, 0, data])
+        time.sleep(1)
+        self.assertRaises(ValueError, tbc.get_histograms, "", "bar")
+
+    def test_get_histograms_wrong_variable(self):
+        tbc = TBClient()
+        data = {"min": 0,
+                "max": 100,
+                "num": 3,
+                "bucket_limit": [10, 50, 30],
+                "bucket": [5, 45, 25]}
+        tbc.add_histogram("foo", "bar", [0, 0, data])
+        time.sleep(1)
+        self.assertRaises(ValueError, tbc.get_histograms, "foo", "")
