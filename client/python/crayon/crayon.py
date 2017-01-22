36aa569934f3e513f920fb8c759bfbbcae973401
albanD <alban@robots.ox.ac.uk>
2017-01-22 22:27:47 +0000

    client rework for proper experiment initialization support

--- client/python/crayon/crayon.py
+++ client/python/crayon/crayon.py
@@ -34,62 +34,77 @@ class CrayonClient(object):
             experiments = json.loads(r.text)
         return experiments
 
-    def new_experiment(self, xp):
+    def new_experiment(self, xp, zip_file=None):
         assert(isinstance(xp, str))
+        return CrayonExperiment(xp, self, zip_file)
+
+
+class CrayonExperiment(object):
+
+    def __init__(self, xp, client, zip_file=None):
+        self.client = client
+        self.xp = xp
+
+        if zip_file:
+            self.init_from_file(zip_file, True)
+        else:
+            self.init_empty()
+
+    def init_empty(self):
         query = "/data"
-        r = requests.post(self.url + query, json=xp)
+        r = requests.post(self.client.url + query, json=self.xp)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
 
-    def add_scalar(self, xp, name, data):
+    def init_from_file(self, zip_file, force=False):
+        query = "/backup?xp={}&force={}".format(
+            self.xp, force)
+        fileobj = open(zip_file, 'rb')
+        r = requests.post(self.client.url + query, data={"mysubmit": "Go"},
+                          files={"archive": ("backup.zip", fileobj)})
+        if not r.ok:
+            raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
+
+    def add_scalar(self, name, data):
         assert(len(data) == 3)
-        query = "/data/scalars?xp={}&name={}".format(xp, name)
-        r = requests.post(self.url + query, json=data)
+        query = "/data/scalars?xp={}&name={}".format(self.xp, name)
+        r = requests.post(self.client.url + query, json=data)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
 
-    def get_scalars(self, xp, name):
-        query = "/data/scalars?xp={}&name={}".format(xp, name)
-        r = requests.get(self.url + query)
+    def get_scalars(self, name):
+        query = "/data/scalars?xp={}&name={}".format(self.xp, name)
+        r = requests.get(self.client.url + query)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
         return json.loads(r.text)
 
-    def add_histogram(self, xp, name, data, tobuild=False):
+    def add_histogram(self, name, data, tobuild=False):
         assert(len(data) == 3)
 
         if tobuild and (not isinstance(data[2], dict)
                         or not self.check_histogram_data(data[2], tobuild)):
             raise ValueError("Data was not provided in a valid format!")
         query = "/data/histograms?xp={}&name={}&tobuild={}".format(
-            xp, name, tobuild)
-        r = requests.post(self.url + query, json=data)
+            self.xp, name, tobuild)
+        r = requests.post(self.client.url + query, json=data)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
 
-    def get_histograms(self, xp, name):
-        query = "/data/histograms?xp={}&name={}".format(xp, name)
-        r = requests.get(self.url + query)
+    def get_histograms(self, name):
+        query = "/data/histograms?xp={}&name={}".format(self.xp, name)
+        r = requests.get(self.client.url + query)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
         return json.loads(r.text)
 
-    def set_data(self, xp, zip_file, force=False):
-        query = "/backup?xp={}&force={}".format(
-            xp, force)
-        fileobj = open(zip_file, 'rb')
-        r = requests.post(self.url + query, data={"mysubmit": "Go"},
-                          files={"archive": ("backup.zip", fileobj)})
-        if not r.ok:
-            raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
-
-    def get_data(self, xp, filename=None):
-        query = "/backup?xp={}".format(xp)
-        r = requests.get(self.url + query)
+    def get_data(self, filename=None):
+        query = "/backup?xp={}".format(self.xp)
+        r = requests.get(self.client.url + query)
         if not r.ok:
             raise ValueError("Something went wrong. Server sent: {}.".format(r.text))
         if not filename:
-            filename = "backup_" + xp + "_" + str(time.time())
+            filename = "backup_" + self.xp + "_" + str(time.time())
         out = open(filename + ".zip", "w")
         out.write(r.content)
         out.close()
--- client/python/test/test_crayon.py
+++ client/python/test/test_crayon.py
@@ -27,91 +27,82 @@ class CrayonClientTestSuite(unittest.TestCase):
     def test_init_wrong_port(self):
         self.assertRaises(ValueError, CrayonClient, "localhost", 123412341234)
 
+    def test_init_xp_empty(self):
+        cc = CrayonClient()
+        self.assertRaises(ValueError, cc.new_experiment, "")
+
     # scalars
     def test_add_scalar(self):
         cc = CrayonClient()
         data = [time.clock(), 1, 2]
-        cc.new_experiment("foo")
-        cc.add_scalar("foo", "bar", data)
+        foo = cc.new_experiment("foo")
+        foo.add_scalar("bar", data)
 
     def test_add_scalar_less_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = []
-        self.assertRaises(AssertionError, cc.add_scalar,
-                          "foo", "bar", data)
+        self.assertRaises(AssertionError, foo.add_scalar,
+                          "bar", data)
 
     # TODO These should really be tested singularly...
     def test_add_scalar_wrong_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = ["lol", "lulz", "lelz"]
-        self.assertRaises(ValueError, cc.add_scalar,
-                          "foo", "bar", data)
-
-    def test_add_scalar_wrong_experiment(self):
-        cc = CrayonClient()
-        data = [time.clock(), 1, 2]
-        self.assertRaises(ValueError, cc.add_scalar,
-                          "", "bar", data)
+        self.assertRaises(ValueError, foo.add_scalar,
+                          "bar", data)
 
     def test_add_scalar_wrong_variable(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
-        self.assertRaises(ValueError, cc.add_scalar,
-                          "foo", "", data)
+        self.assertRaises(ValueError, foo.add_scalar,
+                          "", data)
 
     def test_get_scalars_no_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        self.assertRaises(ValueError, cc.get_scalars, "foo", "bar")
+        foo = cc.new_experiment("foo")
+        self.assertRaises(ValueError, foo.get_scalars, "bar")
 
     def test_get_scalars_one_datum(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        cc.add_scalar("foo", "bar", [0, 0, 0])
+        foo = cc.new_experiment("foo")
+        foo.add_scalar("bar", [0, 0, 0])
         time.sleep(1)
-        self.assertEqual(cc.get_scalars("foo", "bar"), [[0.0, 0, 0.0]])
+        self.assertEqual(foo.get_scalars("bar"), [[0.0, 0, 0.0]])
 
     def test_get_scalars_two_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        cc.add_scalar("foo", "bar", [0, 0, 0])
-        cc.add_scalar("foo", "bar", [1, 1, 1])
+        foo = cc.new_experiment("foo")
+        foo.add_scalar("bar", [0, 0, 0])
+        foo.add_scalar("bar", [1, 1, 1])
         time.sleep(1)
-        self.assertEqual(cc.get_scalars("foo", "bar"),
+        self.assertEqual(foo.get_scalars("bar"),
                          [[0.0, 0, 0.0], [1.0, 1, 1.0]])
-
-    def test_get_scalars_wrong_experiment(self):
-        cc = CrayonClient()
-        cc.new_experiment("foo")
-        cc.add_scalar("foo", "bar", [0, 0, 0])
-        time.sleep(1)
-        self.assertRaises(ValueError, cc.get_scalars, "", "bar")
 
     def test_get_scalars_wrong_variable(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        cc.add_scalar("foo", "bar", [0, 0, 0])
+        foo = cc.new_experiment("foo")
+        foo.add_scalar("bar", [0, 0, 0])
         time.sleep(1)
-        self.assertRaises(ValueError, cc.get_scalars, "foo", "")
+        self.assertRaises(ValueError, foo.get_scalars,"")
 
     # Histograms
     def test_add_histogram(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [0, 1, data])
+        foo.add_histogram("bar", [0, 1, data])
 
     def test_add_histogram_with_sum(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
         data = {"min": 0,
                 "max": 100,
@@ -119,11 +110,11 @@ class CrayonClientTestSuite(unittest.TestCase):
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25],
                 "sum": 75}
-        cc.add_histogram("foo", "bar", [0, 1, data])
+        foo.add_histogram("bar", [0, 1, data])
 
     def test_add_histogram_with_sumsq(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
         data = {"min": 0,
                 "max": 100,
@@ -131,11 +122,11 @@ class CrayonClientTestSuite(unittest.TestCase):
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25],
                 "sum_squares": 5625}
-        cc.add_histogram("foo", "bar", [0, 1, data])
+        foo.add_histogram("bar", [0, 1, data])
 
     def test_add_histogram_with_sum_sumsq(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
         data = {"min": 0,
                 "max": 100,
@@ -144,60 +135,50 @@ class CrayonClientTestSuite(unittest.TestCase):
                 "bucket": [5, 45, 25],
                 "sum": 75,
                 "sum_squares": 2675}
-        cc.add_histogram("foo", "bar", [0, 1, data])
+        foo.add_histogram("bar", [0, 1, data])
 
     def test_add_histogram_less_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = {"some data": 0}
-        self.assertRaises(AssertionError, cc.add_histogram,
-                          "foo", "bar", [data])
+        self.assertRaises(AssertionError, foo.add_histogram,
+                          "bar", [data])
 
     # TODO These should really be tested singularly...
     def test_add_histogram_wrong_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = ["lolz", "lulz", "lelz"]
-        self.assertRaises(ValueError, cc.add_histogram,
-                          "foo", "bar", [0, 1, data], True)
-
-    def test_add_histogram_wrong_experiment(self):
-        cc = CrayonClient()
-        data = {"min": 0,
-                "max": 100,
-                "num": 3,
-                "bucket_limit": [10, 50, 30],
-                "bucket": [5, 45, 25]}
-        self.assertRaises(ValueError, cc.add_histogram,
-                          "", "bar", [0, 1, data])
+        self.assertRaises(ValueError, foo.add_histogram,
+                          "bar", [0, 1, data], True)
 
     def test_add_histogram_wrong_variable(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        self.assertRaises(ValueError, cc.add_histogram,
-                          "foo", "", [0, 1, data])
+        self.assertRaises(ValueError, foo.add_histogram,
+                          "", [0, 1, data])
 
     def test_get_histograms_no_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        self.assertRaises(ValueError, cc.get_histograms, "foo", "bar")
+        foo = cc.new_experiment("foo")
+        self.assertRaises(ValueError, foo.get_histograms, "bar")
 
     def test_get_histograms_one_datum(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [0, 0, data])
+        foo.add_histogram("bar", [0, 0, data])
         time.sleep(1)
-        self.assertEqual(cc.get_histograms("foo", "bar"),
+        self.assertEqual(foo.get_histograms("bar"),
                          [[0.0, 0,
                            [0.0, 100.0, 3.0, 0.0, 0.0,
                             [10.0, 50.0, 30.0],
@@ -205,21 +186,21 @@ class CrayonClientTestSuite(unittest.TestCase):
 
     def test_get_histograms_two_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [0, 0, data])
+        foo.add_histogram("bar", [0, 0, data])
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [1, 1, data])
+        foo.add_histogram("bar", [1, 1, data])
         time.sleep(1)
-        self.assertEqual(cc.get_histograms("foo", "bar"),
+        self.assertEqual(foo.get_histograms("bar"),
                          [[0.0, 0,
                            [0.0, 100.0, 3.0, 0.0, 0.0,
                             [10.0, 50.0, 30.0],
@@ -229,78 +210,53 @@ class CrayonClientTestSuite(unittest.TestCase):
                             [10.0, 50.0, 30.0],
                             [5.0, 45.0, 25.0]]]])
 
-    def test_get_histograms_wrong_experiment(self):
-        cc = CrayonClient()
-        cc.new_experiment("foo")
-        data = {"min": 0,
-                "max": 100,
-                "num": 3,
-                "bucket_limit": [10, 50, 30],
-                "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [0, 0, data])
-        time.sleep(1)
-        self.assertRaises(ValueError, cc.get_histograms, "", "bar")
-
     def test_get_histograms_wrong_variable(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = {"min": 0,
                 "max": 100,
                 "num": 3,
                 "bucket_limit": [10, 50, 30],
                 "bucket": [5, 45, 25]}
-        cc.add_histogram("foo", "bar", [0, 0, data])
+        foo.add_histogram("bar", [0, 0, data])
         time.sleep(1)
-        self.assertRaises(ValueError, cc.get_histograms, "foo", "")
+        self.assertRaises(ValueError, foo.get_histograms, "")
 
     # Only checks that we get a zip file.
     # TODO open and match data to recorded
     def test_get_data(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
-        cc.add_scalar("foo", "bar", data)
+        foo.add_scalar("bar", data)
         time.sleep(1)
-        filename = cc.get_data("foo")
+        filename = foo.get_data()
         os.remove(filename)
 
-    def test_get_data_empty(self):
-        cc = CrayonClient()
-        self.assertRaises(ValueError, cc.get_data, "foo")
-
-    def test_get_data_wrong_experiment(self):
-        cc = CrayonClient()
-        cc.new_experiment("foo")
-        data = [time.clock(), 1, 2]
-        cc.add_scalar("foo", "bar", data)
-        time.sleep(1)
-        self.assertRaises(ValueError, cc.get_data, "not_there")
-
     # Only checks that we set a zip file.
-    def test_set_data(self):
+    def test_init_from_file(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
-        cc.add_scalar("foo", "bar", data)
+        foo.add_scalar("bar", data)
         time.sleep(1)
-        filename = cc.get_data("foo")
+        filename = foo.get_data()
         time.sleep(1)
-        cc.set_data("foo", filename, True)
+        new = cc.new_experiment("new", filename)
         os.remove(filename)
 
     def test_set_data_force(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
+        foo = cc.new_experiment("foo")
         data = [time.clock(), 1, 2]
-        cc.add_scalar("foo", "bar", data)
+        foo.add_scalar("bar", data)
         time.sleep(1)
-        filename = cc.get_data("foo")
+        filename = foo.get_data()
         time.sleep(1)
-        cc.set_data("foo", filename, True)
+        foo.init_from_file(filename, True)
         os.remove(filename)
 
     def test_set_data_wrong_file(self):
         cc = CrayonClient()
-        cc.new_experiment("foo")
-        self.assertRaises(IOError, cc.set_data, "foo",
+        self.assertRaises(IOError, cc.new_experiment, "foo",
                           "random_noise")
