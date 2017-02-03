968ce28a88fa0a988a2fe08be3bb282c7d15b135
albanD <alban@robots.ox.ac.uk>
2017-02-03 18:59:33 +0000

    more tensorboard patching

--- server/patch_tensorboard.py
+++ server/patch_tensorboard.py
@@ -4,54 +4,84 @@ from tensorflow.tensorboard import tensorboard
 import os
 import sys
 
-if len(sys.argv) > 1:
-  new_delay = sys.argv[1]
-else:
-  new_delay = "10"
+frontend_reload = "10"
+backend_reload = "0.1"
 
-print("Patching tensorboard to change the delay to "+new_delay+"s")
+for arg in sys.argv:
+  arg = arg.split("=")
+  if arg[0] == "frontend_reload":
+    frontend_reload = arg[1]
+  elif arg[0] == "backend_reload":
+    backend_reload = arg[1]
+
 
 tb_path = os.path.dirname(os.path.abspath(tensorboard.__file__))
-dist_path = os.path.join(tb_path, "dist")
-html_path = os.path.join(dist_path, "tf-tensorboard.html")
 
-content = []
-state = 0
-# state:
-# 0: looking for variable name
-# 1: looking for type
-# 2: looking for value
-# 3: finishing to read the file
-with open(html_path, "r") as html_file:
-  for line in html_file:
-    if state == 0:
-      if "autoReloadIntervalSecs:" in line:
-        state = 1
-    elif state == 1:
-      if "type: Number" in line:
-        state = 2
+try:
+  print("Patching tensorboard to change the delay to "+frontend_reload+"s")
+
+  dist_path = os.path.join(tb_path, "dist")
+  html_path = os.path.join(dist_path, "tf-tensorboard.html")
+
+  content = []
+  state = 0
+  # state:
+  # 0: looking for variable name
+  # 1: looking for type
+  # 2: looking for value
+  # 3: finishing to read the file
+  with open(html_path, "r") as html_file:
+    for line in html_file:
+      if state == 0:
+        if "autoReloadIntervalSecs:" in line:
+          state = 1
+      elif state == 1:
+        if "type: Number" in line:
+          state = 2
+        else:
+          print("This should not happen, trying to find a new instance")
+          state = 0
+      elif state == 2:
+        if "value: 120" in line:
+          line = line.replace("120", frontend_reload)
+          state = 3
+        else:
+          print("This should not happen, trying to find a new instance")
+          state = 0
+      elif state == 3:
+        pass
       else:
-        print("This should not happen, trying to find a new instance")
-        state = 0
-    elif state == 2:
-      if "value: 120" in line:
-        line = line.replace("120", new_delay)
-        state = 3
-      else:
-        print("This should not happen, trying to find a new instance")
-        state = 0
-    elif state == 3:
-      pass
-    else:
-        print("This should not happen, trying to find a new instance")
-        state = 0
+          print("This should not happen, trying to find a new instance")
+          state = 0
 
-    content += [line]
+      content += [line]
 
-with open(html_path, "w") as html_file:
-  html_file.write("\n".join(content))
+  with open(html_path, "w") as html_file:
+    html_file.write("\n".join(content))
 
-print("Success !")
+  print("Success !")
+except:
+  print("Patching failed")
 
-# except:
-#   print("Tensorboard patching failed")
+try:
+  print("Patching tensorboard to change backend reload to float.")
+
+  tensorboard_path = os.path.join(tb_path, "tensorboard.py")
+
+  content = []
+  with open(tensorboard_path, "r") as source_file:
+    for line in source_file:
+      if ("DEFINE_integer" in line) and ("reload_interval" in line):
+        line = line.replace("DEFINE_integer", "DEFINE_float")
+      content += [line]
+
+  with open(tensorboard_path, "w") as source_file:
+    source_file.write("\n".join(content))
+
+  print("Patch succeded, changing env variable to '"+backend_reload+"'")
+
+  os.environ["BACKEND_RELOAD"]
+
+  print("Success !")
+except:
+  print("Patching failed")
--- server/startup.sh
+++ server/startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Try to patch tensorboard to reduce autorefresh time
-python /patch_tensorboard.py $1
+# Try to patch tensorboard to reduce autorefresh time and allow subsecond backend refresh
+# This will be changed by the patch script if the backend supports subsecond refresh
+export BACKEND_RELOAD="1"
+python /patch_tensorboard.py $1 $2
 
-tensorboard --reload_interval 1 --logdir /tmp/tensorboard --port 8888 2>&1 1>/tmp/tensorboard.log &
+tensorboard --reload_interval $BACKEND_RELOAD --logdir /tmp/tensorboard --port 8888 &
 
 python /server.py 8889 2>&1 1>/tmp/crayon.log
